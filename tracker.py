5f3d55c63f57a598a5ac908f0f7a4965a3f94e6b
Sahib <sahibpsingh13@gmail.com>
2026-05-05 00:40:11 +0530

    tuning the tracker

--- tracker.py
+++ tracker.py
@@ -4,7 +4,7 @@ from deep_sort_realtime.deepsort_tracker import DeepSort
 
 class Tracker:
     def __init__(self):
-        self.tracker = DeepSort(max_age=30)
+        self.tracker = DeepSort(max_age=50, n_init=3)
 
     def update(self, detections, frame):
         tracks = self.tracker.update_tracks(detections, frame=frame)
